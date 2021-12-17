67e7fb9a93e6a48ea4cb62134f253e9d6b212bb5
Sven Nõmm <sven@sensusq.com>
2021-12-17 12:19:48 +0200

    Fixing uncommited stuff.

---
+++ main_transformer.py
@@ -0,0 +1,31 @@
+# this is the the main file for transformer case the spectral project
+# NB case of the intial dataset.
+# for the new data function initial data formatting should be changed
+
+import pandas as pd
+
+#from preprocessing_module import initial_formatting_1  # Reading and formating of the initial data
+from preprocessing_module import initial_formatting_2  # use this line for the second generation of the  data set
+from preprocessing_module import down_sample # this function equalizes the lengthes of the sequences
+from dnn_regression import lstm_wrapper
+from transformers_support import transformer_workflow
+
+#path = '/Users/sven/kohalikTree/Data/AIRSCS/spectral/'
+path = '/Users/sven/kohalikTree/Data/AIRSCS/spectral_1/'  # to read data submitted on 10.12.2021
+
+#model_order = 55
+#load the files
+#initial_data_file = path + 'sarspec_X.csv'
+#target_data_file = path + 'lainespec_Y.csv'
+
+initial_data_file = path + 'sarspec_hgh_clean.csv' # this and the following row are for the data 10.12.2021
+target_data_file = path + 'wavespec_hgh_clean.csv'
+
+initial_data = pd.read_csv(initial_data_file, sep=',') # keep in mind which separator to use
+target_data = pd.read_csv(target_data_file, sep=',')
+
+initial_data, target_data = initial_formatting_2(initial_data, target_data)
+#initial_data = down_sample(initial_data, target_data)
+bs = transformer_workflow(initial_data, target_data)
+
+print("That's all folks!!!")
--- transformers_support.py
+++ transformers_support.py
@@ -1,8 +1,9 @@
 # this file contains the functions necessary for the transformers support
-
+import time
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from preprocessing_module import splitting_wrapper
 
 def get_angles(pos, i, d_model):
     angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
@@ -235,6 +236,7 @@ class Encoder(tf.keras.layers.Layer):
 
         return x  # (batch_size, input_seq_len, d_model)
 
+
 class Decoder(tf.keras.layers.Layer):
     def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size,
                      maximum_position_encoding, rate=0.1):
@@ -356,12 +358,30 @@ def accuracy_function(real, pred):
     return tf.reduce_sum(accuracies)/tf.reduce_sum(mask)
 
 
-def workflow(training_data_x, training_data_y):
+def tokenize_pairs(input_data, output_data):
+    input_data = input_data.to_tensor()
+    output_data = output_data.to_tensor()
+    return input_data, output_data
+
+def make_batches(ds):
+    BUFFER_SIZE = 20000
+    BATCH_SIZE = 64
+    return (
+        ds
+        .cache()
+        .shuffle(BUFFER_SIZE)
+        .batch(BATCH_SIZE)
+        .map(tokenize_pairs, num_parallel_calls=tf.data.AUTOTUNE)
+        .prefetch(tf.data.AUTOTUNE))
+
+def transformer_workflow(initial_data, target_data):
+    initial_data_train, initial_data_test, target_data_train, target_data_test = splitting_wrapper(initial_data,
+                                                                                                   target_data)
     # logging.getLogger('tensorflow').setLevel(logging.ERROR)
     BUFFER_SIZE = 20000
     BATCH_SIZE = 64
-    #train_batches = make_batches(train_examples)
-    #val_batches = make_batches(val_examples)
+    train_batches = make_batches(train_examples)
+    val_batches = make_batches(val_examples)
     n, d = 2048, 512
     pos_encoding = positional_encoding(n, d)
     print(pos_encoding.shape)
@@ -377,3 +397,94 @@ def workflow(training_data_x, training_data_y):
     plt.xlabel('Position')
     plt.colorbar()
     plt.show()
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
+
+    num_layers = 4
+    d_model = 128
+    dff = 512
+    num_heads = 8
+    dropout_rate = 0.1
+
+    transformer = Transformer(
+        num_layers=num_layers,
+        d_model=d_model,
+        num_heads=num_heads,
+        dff=dff,
+        input_vocab_size=1000,  #tokenizers.pt.get_vocab_size().numpy(),
+        target_vocab_size=1000,  #tokenizers.en.get_vocab_size().numpy(),
+        pe_input=1000,
+        pe_target=1000,
+        rate=dropout_rate)
+
+    checkpoint_path = "./checkpoints/train"
+
+    learning_rate = CustomSchedule(d_model)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                         epsilon=1e-9)
+    ckpt = tf.train.Checkpoint(transformer=transformer,
+                               optimizer=optimizer)
+
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+    # if a checkpoint exists, restore the latest checkpoint.
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
+
+    temp_learning_rate_schedule = CustomSchedule(d_model)
+
+    plt.plot(temp_learning_rate_schedule(tf.range(40000, dtype=tf.float32)))
+    plt.ylabel("Learning Rate")
+    plt.xlabel("Train Step")
+
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+
+    EPOCHS = 20
+    train_step_signature = [
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    ]
+
+    @tf.function(input_signature=train_step_signature)
+    def train_step(inp, tar):
+        tar_inp = tar[:, :-1]
+        tar_real = tar[:, 1:]
+
+        with tf.GradientTape() as tape:
+            predictions, _ = transformer([inp, tar_inp],
+                                         training=True)
+            loss = loss_function(tar_real, predictions)
+
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(accuracy_function(tar_real, predictions))
+
+    for epoch in range(EPOCHS):
+        start = time.time()
+
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        # inp -> portuguese, tar -> english
+        for (batch, (inp, tar)) in enumerate(train_batches):
+            train_step(inp, tar)
+
+            if batch % 50 == 0:
+                print(
+                    f'Epoch {epoch + 1} Batch {batch} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
+        if (epoch + 1) % 5 == 0:
+            ckpt_save_path = ckpt_manager.save()
+            print(f'Saving checkpoint for epoch {epoch + 1} at {ckpt_save_path}')
+
+        print(f'Epoch {epoch + 1} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
+        print(f'Time taken for 1 epoch: {time.time() - start:.2f} secs\n')
+
+    return BATCH_SIZE
